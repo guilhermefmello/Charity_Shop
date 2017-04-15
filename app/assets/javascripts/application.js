8769972a6539d7b0d5a636fbd4fc8684929bdeff
Alex Santos <xade.26@hotmail.com>
2017-04-15 11:18:03 +0100

    list of product are displayed as grid layout and user have option to change to list layout view. Javascript was implemented with bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,3 +27,8 @@ function closeNav() {
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
 
+// View en grid et liste pour categories
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#categories-list .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#categories-list .item').removeClass('list-group-item');$('#categories-list .item').addClass('grid-group-item');});
+});
